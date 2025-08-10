8fb0bfc72e5d202017d79b317ce9d9c501186819
cbx-nz <christmastvonlineclothes@gmail.com>
2025-08-10 21:09:27 +1200

    yes

--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    // Remove reusePort option as it's not supported on Windows
   }, () => {
     log(`serving on port ${port}`);
   });
